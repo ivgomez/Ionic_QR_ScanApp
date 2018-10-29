f6550a5cbae56adca8fed5093c95c8cac01b43e0
Ivan Dario <iv.gomez@hotmail.com>
2018-10-29 14:26:48 -0500

    first commit

---
+++ src/pages/mapa/mapa.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-mapa',
+  templateUrl: 'mapa.html',
+})
+export class MapaPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MapaPage');
+  }
+
+}
